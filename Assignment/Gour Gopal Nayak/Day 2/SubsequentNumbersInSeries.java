6eed3b6ca0bdaa0e7e278a9ddff8a0f9dd33582d
Gour-subh <gourgopal2023@gift.edu.in>
2024-07-22 12:33:16 +0530

    Add files via upload

---
+++ Assignment/Gour Gopal Nayak/Day 2/SubsequentNumbersInSeries.java
@@ -0,0 +1,16 @@
+
+public class SubsequentNumbersInSeries {
+
+	public static void main(String[] args) {
+	
+		  int currentNumber = 1;
+	        System.out.println("First 10 subsequent numbers in the series:");
+	        System.out.print(currentNumber + " ");
+	        for (int i = 1; i <= 10; i++) {
+	            currentNumber *= 2; 
+	            System.out.print(currentNumber + " ");
+		
+	}
+
+}
+}
