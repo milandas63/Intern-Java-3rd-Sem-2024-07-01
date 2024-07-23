09e6ee7f46bf571a1dcb18e0fae2c8e565d31c07
Sumit8924 <srout2023@gift.edu.in>
2024-07-23 21:26:24 +0530

    Add files via upload

---
+++ Project/Group-11 (VideoGallery)/VideoPlayer.java
@@ -0,0 +1,38 @@
+package com.example.videogallery;
+
+public class VideoPlayer {
+
+public class VideoGallery {
+
+public class Video {
+    private String title;
+    private String url;
+
+    public Video(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
+// Create more classes as needed (e.g., VideoGallery, VideoPlayer, etc.)
+
+}
+
+}
